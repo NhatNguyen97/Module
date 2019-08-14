c2ab94176ff01533b58868eda8c759c92f4e16c3
NhatNguyen97 <vnhat98@gmail.com>
2019-08-14 20:02:28 +0100

    update

--- sensor_temp/wiringPi_sensor.c
+++ sensor_temp/wiringPi_sensor.c
@@ -2,11 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <sched.h>
+#include <string.h>
 #define MAXTIMINGS	85
 #define DHTPIN		7
-#define DHT_MAXCOUNT 32000
+#define DHT_MAXCOUNT 20000
+#define DHT_MAXCOUNT_0 32000
+#define DHT_MAXCOUNT_1 14500
 #define DHT_PULSES 41
 int dht11_dat[5] = { 0, 0, 0, 0, 0 };
+
+void set_default_priority(void) {
+  struct sched_param sched;
+  memset(&sched, 0, sizeof(sched));
+  // Go back to default scheduler with default 0 priority.
+  sched.sched_priority = 0;
+  sched_setscheduler(0, SCHED_OTHER, &sched);
+}
 
 void read_dht11_dat()
 {
@@ -19,77 +31,49 @@ void read_dht11_dat()
 
 	/* pull pin down for 18 milliseconds */
 	pinMode( DHTPIN, OUTPUT );
-	digitalWrite( DHTPIN, HIGH );
-	delay( 500 );
-	/* then pull it up for 40 microseconds */
 	digitalWrite( DHTPIN, LOW );
 	delay( 20 );
+	/* then pull it up for 40 microseconds */
+	digitalWrite( DHTPIN, HIGH );
+	delayMicroseconds(40);
 	/* prepare to read the pin */
 	pinMode( DHTPIN, INPUT );
-	delayMicroseconds( 50 );
-	/* detect change and read data */
-	/* for ( i = 0; i < MAXTIMINGS; i++ )
-	{
-		counter = 0;
-		while ( digitalRead( DHTPIN ) == laststate )
-		{
-			counter++;
-			delayMicroseconds( 1 );
-			if ( counter == 255 )
-			{
-				break;
-			}
-		}
-		laststate = digitalRead( DHTPIN );
-		printf("%d",laststate);
-		//if ( counter == 255 )
-		//	break;
-
-		// ignore first 3 transitions 
-		if ( (i >= 4) && (i % 2 == 0) )
-		{
-			// shove each bit into the storage bytes 
-			dht11_dat[j / 8] <<= 1;
-			if ( counter > 16 )
-				dht11_dat[j / 8] |= 1;
-			j++;
-		}
-	 } */
-	laststate = digitalRead(DHTPIN);
-	printf("last state: %d\n",laststate);	 
-	while( 1 == digitalRead(DHTPIN))
-	{
-		if(++count >= DHT_MAXCOUNT)
-		{
-			printf("Failed to low 1\n");
-		}
-		
-	}
+	digitalWrite(DHTPIN,LOW);
+	delayMicroseconds(80);
+	
 	for (int i=0; i < DHT_PULSES*2; i+=2)
 	{
 		while(0 == digitalRead(DHTPIN))
 		{
-			if(++pulseCounts[i] >= DHT_MAXCOUNT)
+			if(++pulseCounts[i] >= DHT_MAXCOUNT_0)
 			{
-				printf("Failed to high %d\n",i);
-				break;
+				//set_default_priority();
+				printf("Failed to h1\n");
+				return -1;
 			}
-		}	
+		
+		}
 		while(1 == digitalRead(DHTPIN))
 		{
-			if(++pulseCounts[i+1] >= DHT_MAXCOUNT)
+			if(++pulseCounts[i+1] >= DHT_MAXCOUNT_0)
 			{
-				printf("Failed to high %d\n");
-				break;
+				pulseCounts[i+1] = DHT_MAXCOUNT_0;
+				return -1;
+				//set_default_priority();
+				//printf("Failed to l %d c: %d\n",i+1,pulseCounts[i+1]);
+				//return -1;
+				
 			}
-		}	
+		}
 	}
+	set_default_priority();
  	int  threshold = 0;
 	for (i=2; i < DHT_PULSES*2; i+=2) {
 		threshold += pulseCounts[i];
 	}
-
+	printf("threshhold: %d\n",threshold);
 	threshold /= DHT_PULSES-1;
+	printf("threshhold: %d\n",threshold);
 	for (i=3; i < DHT_PULSES*2; i+=2) {
 		int index = (i-3)/16;
 		dht11_dat[index] <<= 1;
@@ -123,7 +107,7 @@ int main( void )
 	while ( 1 )
 	{
 		read_dht11_dat();
-		delay( 2000 ); /* wait 1sec to refresh */
+		delay( 1000 ); /* wait 1sec to refresh */
 	}
 
 	return(0);
